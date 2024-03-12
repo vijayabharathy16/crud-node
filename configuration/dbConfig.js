9c645d0485d017f9fcf52b4c7236aa47e8fec7c3
vijayabharathy16 <vijayabharathy753@gmail.com>
2024-03-12 17:09:51 +0530

    initial code

---
+++ configuration/dbConfig.js
@@ -0,0 +1,22 @@
+const mongoose = require('mongoose');
+
+mongoose.connect("mongodb://localhost:27017/crud_db",{
+});
+
+mongoose.connection.on("connected", () =>{
+    console.log("Connected to MongoDB");
+});
+
+mongoose.connection.on("error", (err) =>{
+    console.log(`MongoDB connection error: ${err}`);
+});
+// const connectedToMongoDB = async () => {
+//     try {
+//        await mongoose.connect("mongodb://localhost:27017/crud_db");
+//        console.log("Connected to MongoDB");
+//     } catch (error) {
+//         console.log(`MongoDB connection error: ${error}`);
+//     }
+// }
+
+module.exports = mongoose;
