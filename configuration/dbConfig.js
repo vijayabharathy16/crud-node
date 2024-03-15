fba44b5aec267ecaa32bd8161be20e27bdc7ffb8
vijayabharathy16 <vijayabharathy753@gmail.com>
2024-03-15 14:12:18 +0530

    initial

--- configuration/dbConfig.js
+++ configuration/dbConfig.js
@@ -1,6 +1,9 @@
 const mongoose = require('mongoose');
 
-mongoose.connect("mongodb://localhost:27017/crud_db",{
+// mongoose.connect("mongodb://localhost:27017/crud_db",{
+// });
+
+mongoose.connect("mongodb+srv://crud-node:OyB8WYk1jvBgzWPM@cluster0.ecywzex.mongodb.net/crud-db?retryWrites=true&w=majority&appName=Cluster0",{
 });
 
 mongoose.connection.on("connected", () =>{
